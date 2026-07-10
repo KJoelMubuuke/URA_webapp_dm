64f8f959db091fb283311db1d7acf73117fc4af2
KJoelMubuuke <joelchrt75@gmail.com>
2026-07-10 12:40:01 +0300

    added functionality tio automate database creation if not exist

--- src/main/java/org/ura/util/HibernateUtil.java
+++ src/main/java/org/ura/util/HibernateUtil.java
@@ -3,15 +3,32 @@ package org.ura.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+        createDatabaseIfNotExists();
         return new Configuration().configure().buildSessionFactory();
+    }
+
+    private static void createDatabaseIfNotExists() {
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String password = "light";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS ura_tax_db");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to auto-create database", e);
+        }
     }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 }
-
